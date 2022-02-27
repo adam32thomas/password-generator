73f3220ba56a317fa187395ebd59b2e694c7df4b
AJDewy <adam32thomas@gmail.com>
2022-02-27 18:17:03 -0500

    define requirements

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,24 @@
 // Assignment code here
+//Define variables
+//let numbers " 1234567890"
+//let letters uppercase "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//let letters lowercase "abcdefghijklmnopqrstuvwxyz"
+//let special characters " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+//let length equal 8 to 128 characters
+//ask length
+//ask if letters uppercase
+//ask if letters lowercase
+//ask if numbers
+//ask if special characters
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
